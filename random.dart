cc8a9cd5f175f5b4940e36419d995ffd4c7d7089
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-02-09 00:28:28 +0500

    addedd

--- random.dart
+++ random.dart
@@ -3,12 +3,17 @@ import "dart:math";
 
 void main() {
   Random ran = Random();
-  int random = ran.nextInt(2);
-  print("Enter number");
-  int input = int.parse(stdin.readLineSync()!);
-  print(random);
-  if (input == random) {
-    print("You win");
+  int random = ran.nextInt(10);
+  while (true) {
+    print("Enter number or 'q' to quit");
+    var input = int.parse(stdin.readLineSync()!);
+    if (input == "q") {
+      break;
+    }
+    print(random);
+    if (input == random) {
+      print("You win");
+    }
+    print("You Lose");
   }
-  print("You Lose");
 }
