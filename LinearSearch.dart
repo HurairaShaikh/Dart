9d666e155b8904794b860ca8774c3946948cbf70
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-02-10 00:39:37 +0500

    Linear Search Added

---
+++ LinearSearch.dart
@@ -0,0 +1,14 @@
+
+//import 'dart:io';
+
+void main() {
+  List as = ["asif","bilal","zebra","donkey","money","cartoon","anna"];
+  String x = "anna";
+  for (int i = 0; i < as.length; i++) {
+    if (x == as[i]) {
+      print('found at index $i');
+    }
+  }
+}
+
+
