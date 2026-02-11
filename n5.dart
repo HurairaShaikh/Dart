43163502e7cb057414e76f9896eb6f5e165d1133
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-02-11 23:49:37 +0500

    alpha PAtteren

---
+++ n5.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  int n = 3;
+  int char = 65;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      stdout.write(String.fromCharCode(char));
+      char=char+1;
+    }
+    stdout.write("\n");
+  }
+}
